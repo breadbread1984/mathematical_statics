e6f50f4e5887628d1bf22ba2bbd09486fee9e2f8
breadbread1984 <breadbread1984@163.com>
2022-02-20 19:09:45 +0800

    update

--- parameter_estimation.py
+++ parameter_estimation.py
@@ -6,7 +6,18 @@ def moment(x, order = 1):
   assert type(order) is int;
   return tf.math.reduce_mean(x**order);
 
+def bernoull9i_moment_estimation(x = None, fst_moment = None, snd_moment = None):
+  # NOTE: mean(x) = p, var(x) = p(1-p)
+  # moment(x,1) = mean(x) = p, moment(x,2) = var(x) + mean(x)^2 = p(1-p) + p^2
+  assert x is not None or (fst_moment is not None and snd_moment is not None);
+  moment1 = moment(x, 1) if x is not None else fst_moment;
+  moment2 = moment(x, 2) if x is not None else snd_moment;
+  p = moment1;
+  return p;
+
 def uniform_moment_estimation(x = None, fst_moment = None, snd_moment = None):
+  # NOTE: mean(x) = (a+b)/2, var(x) = (b-a)^2/12
+  # moment(x,1) = mean(x) = (a+b)/2, moment(x,2) = var(x) + mean(x)^2 = (b-a)^2/12 + (a+b)^2/4
   assert x is not None or (fst_moment is not None and snd_moment is not None);
   moment1 = moment(x, 1) if x is not None else fst_moment;
   moment2 = moment(x, 2) if x is not None else snd_moment;
@@ -15,6 +26,8 @@ def uniform_moment_estimation(x = None, fst_moment = None, snd_moment = None):
   return a,b;
 
 def normal_moment_estimation(x = None, fst_moment = None, snd_moment = None):
+  # NOTE: mean(x) = u, var(x) = sigma^2
+  # moment(x,1) = mean(x) = u, moment(x,2) = var(x) + mean(x)^2 = sigma^2 + u^2
   assert x is not None or (fst_moment is not None and snd_moment is not None);
   moment1 = moment(x, 1) if x is not None else fst_moment;
   moment2 = moment(x, 2) if x is not None else snd_moment;
